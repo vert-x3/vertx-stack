aba39cf3dae30dd8c7a64bdeca4571e95f3619fc
purplefox <timvolpe@gmail.com>
2015-02-17 11:31:01 +0000

    Removed sockjs and updated vertxbus.js

--- stack-dist/src/main/client/vertxbus.js
+++ stack-dist/src/main/client/vertxbus.js
@@ -1,17 +1,17 @@
 /*
- *   Copyright (c) 2011-2013 The original author or authors
- *   ------------------------------------------------------
- *   All rights reserved. This program and the accompanying materials
- *   are made available under the terms of the Eclipse Public License v1.0
- *   and Apache License v2.0 which accompanies this distribution.
+ * Copyright 2014 Red Hat, Inc.
  *
- *       The Eclipse Public License is available at
- *       http://www.eclipse.org/legal/epl-v10.html
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
  *
- *       The Apache License v2.0 is available at
- *       http://www.opensource.org/licenses/apache2.0.php
+ *  The Eclipse Public License is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
  *
- *   You may elect to redistribute this code under either of these licenses.
+ *  The Apache License v2.0 is available at
+ *  http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
  */
 
 var vertx = vertx || {};
@@ -29,13 +29,12 @@ var vertx = vertx || {};
 }(function(SockJS) {
 
   vertx.EventBus = function(url, options) {
-  
+
     var that = this;
     var sockJSConn = new SockJS(url, undefined, options);
     var handlerMap = {};
     var replyHandlers = {};
     var state = vertx.EventBus.CONNECTING;
-    var sessionID = null;
     var pingTimerID = null;
     var pingInterval = null;
     if (options) {
@@ -44,30 +43,18 @@ var vertx = vertx || {};
     if (!pingInterval) {
       pingInterval = 5000;
     }
-  
+
     that.onopen = null;
     that.onclose = null;
 
-    that.login = function(username, password, replyHandler) {
-      sendOrPub("send", 'vertx.basicauthmanager.login', {username: username, password: password}, function(reply) {
-        if (reply.status === 'ok') {
-          that.sessionID = reply.sessionID;
-        }
-        if (replyHandler) {
-          delete reply.sessionID;
-          replyHandler(reply)
-        }
-      });
-    }
-  
     that.send = function(address, message, replyHandler) {
       sendOrPub("send", address, message, replyHandler)
     }
-  
+
     that.publish = function(address, message) {
       sendOrPub("publish", address, message, null)
     }
-  
+
     that.registerHandler = function(address, handler) {
       checkSpecified("address", 'string', address);
       checkSpecified("handler", 'function', handler);
@@ -78,13 +65,13 @@ var vertx = vertx || {};
         handlerMap[address] = handlers;
         // First handler for this address so we should register the connection
         var msg = { type : "register",
-                    address: address };
+          address: address };
         sockJSConn.send(JSON.stringify(msg));
       } else {
         handlers[handlers.length] = handler;
       }
     }
-  
+
     that.unregisterHandler = function(address, handler) {
       checkSpecified("address", 'string', address);
       checkSpecified("handler", 'function', handler);
@@ -95,25 +82,25 @@ var vertx = vertx || {};
         if (idx != -1) handlers.splice(idx, 1);
         if (handlers.length == 0) {
           // No more local handlers so we should unregister the connection
-  
+
           var msg = { type : "unregister",
-                      address: address};
+            address: address};
           sockJSConn.send(JSON.stringify(msg));
           delete handlerMap[address];
         }
       }
     }
-  
+
     that.close = function() {
       checkOpen();
       state = vertx.EventBus.CLOSING;
       sockJSConn.close();
     }
-  
+
     that.readyState = function() {
       return state;
     }
-  
+
     sockJSConn.onopen = function() {
       // Send the first ping then send a ping every pingInterval milliseconds
       sendPing();
@@ -123,7 +110,7 @@ var vertx = vertx || {};
         that.onopen();
       }
     };
-  
+
     sockJSConn.onclose = function() {
       state = vertx.EventBus.CLOSED;
       if (pingTimerID) clearInterval(pingTimerID);
@@ -131,10 +118,15 @@ var vertx = vertx || {};
         that.onclose();
       }
     };
-  
+
     sockJSConn.onmessage = function(e) {
       var msg = e.data;
       var json = JSON.parse(msg);
+      var type = json.type;
+      if (type === 'err') {
+        console.error("Error received on connection: " + json.body);
+        return;
+      }
       var body = json.body;
       var replyAddress = json.replyAddress;
       var address = json.address;
@@ -169,17 +161,14 @@ var vertx = vertx || {};
       }
       sockJSConn.send(JSON.stringify(msg));
     }
-  
+
     function sendOrPub(sendOrPub, address, message, replyHandler) {
       checkSpecified("address", 'string', address);
       checkSpecified("replyHandler", 'function', replyHandler, true);
       checkOpen();
       var envelope = { type : sendOrPub,
-                       address: address,
-                       body: message };
-      if (that.sessionID) {
-        envelope.sessionID = that.sessionID;
-      }
+        address: address,
+        body: message };
       if (replyHandler) {
         var replyAddress = makeUUID();
         envelope.replyAddress = replyAddress;
@@ -188,13 +177,13 @@ var vertx = vertx || {};
       var str = JSON.stringify(envelope);
       sockJSConn.send(str);
     }
-  
+
     function checkOpen() {
       if (state != vertx.EventBus.OPEN) {
         throw new Error('INVALID_STATE_ERR');
       }
     }
-  
+
     function checkSpecified(paramName, paramType, param, optional) {
       if (!optional && !param) {
         throw new Error("Parameter " + paramName + " must be specified");
@@ -203,16 +192,16 @@ var vertx = vertx || {};
         throw new Error("Parameter " + paramName + " must be of type " + paramType);
       }
     }
-  
+
     function isFunction(obj) {
       return !!(obj && obj.constructor && obj.call && obj.apply);
     }
-  
+
     function makeUUID(){return"xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
         .replace(/[xy]/g,function(a,b){return b=Math.random()*16,(a=="y"?b&3|8:b|0).toString(16)})}
-  
+
   }
-  
+
   vertx.EventBus.CONNECTING = 0;
   vertx.EventBus.OPEN = 1;
   vertx.EventBus.CLOSING = 2;
