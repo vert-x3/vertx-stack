3ec29f91884acc3465056348bd6b04e4408e93a7
Clement Escoffier <clement.escoffier@gmail.com>
2015-12-09 13:46:13 +0100

    Revert the addition of the launcher-class option in the vertx CLI.

--- stack-manager/src/main/packaging/bin/vertx.bat
+++ stack-manager/src/main/packaging/bin/vertx.bat
@@ -99,7 +99,7 @@ set CLASSPATH=%CLASSPATH%;%VERTX_HOME%\conf;%VERTX_HOME%\lib\*
 
 @rem Execute vertx
 "%JAVA_EXE%" %JVM_OPTS% %JMX_OPTS% %JAVA_OPTS% %VERTX_SYNC_AGENT% %VERTX_OPTS% %VERTX_MODULE_OPTS% -Dvertx.cli.usage.prefix=vertx -Djava.util.logging.config.file="%VERTX_JUL_CONFIG%" -Dvertx.home="%VERTX_HOME%" -Dvertx.clusterManagerFactory="%VERTX_CLUSTERMANAGERFACTORY%" -classpath "%CLASSPATH%"
-io.vertx.core.Launcher %CMD_LINE_ARGS% --launcher-class=io.vertx.core.Launcher
+io.vertx.core.Launcher %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
